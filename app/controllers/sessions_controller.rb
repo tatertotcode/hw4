338ee70f583088bfa3ad9158f8a0a63c57324d32
tatertotcode <elijah.kravets@kellogg.northwestern.edu>
2026-03-14 20:49:55 +0000

    login flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,31 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # Find the user by their email address
+    @user = User.find_by({ "email" => params["email"] })
+
+    # If the user exists, check if they know their password
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        # 3. If they know their password, log them in!
+        # We use the session hash to store the user's ID in a browser cookie
+        session["user_id"] = @user["id"]
+        redirect_to "/places"
+      else
+        # Password was incorrect
+        redirect_to "/sessions/new"
+      end
+    else
+      # User didn't exist
+      redirect_to "/sessions/new"
+    end
   end
 
+
   def destroy
+    # Log out the user by clearing the cookie
+    session["user_id"] = nil
+    redirect_to "/sessions/new"
   end
 end
   

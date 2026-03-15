3edc74b9838221b2a256a763218fee15c92ea3d5
tatertotcode <elijah.kravets@kellogg.northwestern.edu>
2026-03-15 17:02:58 +0000

    authorization - only logged in user can see their own places and entries

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,14 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+
+    # filter entries to only show those that match current user and place
+    if @current_user
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
+    else
+      # if no one is logged in, dont show private entries
+      @entries = []
+    end
   end
 
   def new
